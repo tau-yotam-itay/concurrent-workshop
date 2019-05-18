6c80e2b4a4e35c9177a00c2f697096d90de88329
yotammanne <38666487+yotammanne@users.noreply.github.com>
2019-05-18 14:51:16 +0300

    first commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  workshop
+//
+//  Created by Yotam Manne on 18/05/2019.
+//  Copyright © 2019 Yotam Manne. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
