12bc0e8529d19e24a2bf197297d0c2d26b265ce4
itay levi <eitay.levi@gmail.com>
2019-05-22 15:53:12 +0300

    added fields to BH_Node

--- binary_heap.cpp
+++ binary_heap.cpp
@@ -8,6 +8,7 @@ class BH_Node {
 private:
 	int dist;
 	Vertex* v;
+	BH_Node* parent;
 	BH_Node* left;
 	BH_Node* right;
 
@@ -16,6 +17,7 @@ public:
 	BH_Node(Vertex* vertex) {
 		dist = INT_MAX;
 		v = vertex;
+		parent = NULL;
 		left = NULL;
 		right = NULL;
 	}
@@ -24,22 +26,31 @@ public:
 		//maybe call the default constructor instead
 		dist = distance;
 		v = vertex;
+		parent = NULL;
 		left = NULL;
 		right = NULL;
 	}
-
 	int get_dist() {
 		return dist;
 	}
-
 	Vertex* get_vertex() {
 		return v;
 	}
-
+	BH_Node* get_parent(BH_Node* node) {
+		return parent;
+	}
+	BH_Node* get_left(BH_Node* node) {
+		return left;
+	}
+	BH_Node* get_right(BH_Node* node) {
+		return right;
+	}
+	void set_parent(BH_Node* node) {
+		parent = node;
+	}
 	void set_left(BH_Node* node) {
 		left = node;
 	}
-
 	void set_right(BH_Node* node) {
 		right = node;
 	}
@@ -48,14 +59,31 @@ public:
 class Binary_Heap {
 private:
 	BH_Node* min_node;
+	BH_Node* last_node;
+
 //heapify_up,heapify_down
 public:
 	volatile bool lock;
 //constructor, get funcs, extract_min, insert, decrease_key
 
+	Binary_Heap() {
+		min_node = NULL;
+		last_node = NULL;
+	}
+/*
+	BH_Node* get_min() {
+		return min_node;
+	}
+	BH_Node* get_last() {
+		return last_node;
+	}
+*/
 	void insert(BH_Node) {
 
 	}
+
+private:
+
 
 };
 
